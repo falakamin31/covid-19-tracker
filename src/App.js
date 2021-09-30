f796b06f95c9d5235524e35da2506be695270196
Falak Amin <falakamin2000@gmail.com>
2021-09-30 17:07:26 +0500

    API call to fetch data, installed dependency

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react'
+import {Cards, Chart, CountryPicker } from "./components"
+import styles from "./App.module.css"
+import { fetchData } from "./api"
 
 const App = () => {
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      const fetchedData = await fetchData();
+      setData({data: fetchedData});
+    }
+    getData();
+  }, [])
+
   return (
-    <div>
-      
+    <div className={styles.container}>
+      <Cards data={data}/>
+      <CountryPicker />
+      <Chart />
     </div>
   )
-}
 
+}
 export default App
---
+++ src/components/Cards/Cards.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Cards = (props) => {
+    console.log(props);
+    return (
+        <div>
+           Cards 
+        </div>
+    )
+}
+
+export default Cards
